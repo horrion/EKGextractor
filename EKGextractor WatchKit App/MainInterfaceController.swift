a34bd353de837eba27c7837563a9ffdfdccb91f8
Unknown <horrion@me.com>
2021-03-31 23:35:02 +0200

    -Added initial EKG Sample getter function -Added initial voltage measurement function for each sample

--- EKGextractor WatchKit App/MainInterfaceController.swift
+++ EKGextractor WatchKit App/MainInterfaceController.swift
@@ -8,6 +8,7 @@
 
 import WatchKit
 import Foundation
+import HealthKit
 
 
 class MainInterfaceController: WKInterfaceController {
@@ -27,5 +28,83 @@ class MainInterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    @IBAction func extractAction() {
+        
+        // Get the EKG samples from HealthKit
+        getEKGSample()
+        
+    }
+    
+    func getEKGSample() {
+        
+        // Create the electrocardiogram sample type.
+        let ecgType = HKObjectType.electrocardiogramType()
+        
+        // Query for electrocardiogram samples
+        let ecgQuery = HKSampleQuery(sampleType: ecgType,
+                                     predicate: nil,
+                                     limit: HKObjectQueryNoLimit,
+                                     sortDescriptors: nil) { (query, samples, error) in
+            if let error = error {
+                // Handle the error here.
+                fatalError("*** An error occurred \(error.localizedDescription) ***")
+            }
+            
+            guard let ecgSamples = samples as? [HKElectrocardiogram] else {
+                fatalError("*** Unable to convert \(String(describing: samples)) to [HKElectrocardiogram] ***")
+            }
+            
+            for sample in ecgSamples {
+                // Handle the samples here.
+                
+                print("Analyzing sample: ")
+                print(sample)
+                getVoltageMeasurementForSample(ecgSample: sample)
+            }
+        }
+
+        // Execute the query.
+        healthStore.execute(ecgQuery)
+    }
+    
+    
+    func getVoltageMeasurementForSample(ecgSample: HKElectrocardiogram) {
+        
+        // Create a query for the voltage measurements
+        let voltageQuery = HKElectrocardiogramQuery(ecgSample) { (query, result) in
+            switch(result) {
+            
+            case .measurement(let measurement):
+                if let voltageQuantity = measurement.quantity(for: .appleWatchSimilarToLeadI) {
+                    // Do something with the voltage quantity here.
+                    
+                    print("Analyzing voltage measurement: ")
+                    print(voltageQuantity)
+                    
+                    // TODO: Write this data to a csv file
+                    
+                }
+            
+            case .done:
+                // No more voltage measurements. Finish processing the existing measurements.
+                print("Last measurement was processed")
+
+            case .error(let error):
+                // Handle the error here.
+                print("Error occurred during voltage measurement processing")
+                
+            }
+        }
+
+        // Execute the query.
+        healthStore.execute(voltageQuery)
+        
+    }
+    
+    // TODO: Implement csv engine
+    
+    
+    
 
 }
