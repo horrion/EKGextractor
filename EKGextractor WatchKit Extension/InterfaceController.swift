7231b8288288f1b02b160f433dd2f40154fd47e0
Unknown <horrion@me.com>
2021-03-31 23:32:20 +0200

    Initial commit

---
+++ EKGextractor WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  EKGextractor WatchKit Extension
+//
+//  Created by Robert Horrion on 3/31/21.
+//  Copyright © 2021 Robert Horrion. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
